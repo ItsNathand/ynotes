bdb47e9cfb0187721b105b775800ae5e1ff4eae0
Nathan Demers <nathancodes@icloud.com>
2021-08-26 09:26:18 +0200

    changes

--- lib/core/apis/model.dart
+++ lib/core/apis/model.dart
@@ -44,12 +44,13 @@ abstract class API {
   ///Download a file from his name
   Future<Request> downloadRequest(Document document);
 
-
   ///All events
-  Future<List<AgendaEvent>?> getEvents(DateTime date, {bool forceReload = false}) async {
+  Future<List<AgendaEvent>?> getEvents(DateTime date,
+      {bool forceReload = false}) async {
     List<AgendaEvent> events = [];
     List<AgendaEvent>? extracurricularEvents = [];
-    List<Lesson>? lessons = await (appSys.api!.getNextLessons(date, forceReload: forceReload));
+    List<Lesson>? lessons =
+        await (appSys.api!.getNextLessons(date, forceReload: forceReload));
     int week = await getWeek(date);
     //Add lessons for this day
     if (lessons != null) {
@@ -62,15 +63,16 @@ abstract class API {
     RecurringEventSchemes recurr = RecurringEventSchemes();
     recurr.date = date;
     recurr.week = week;
-    var recurringEvents = await AgendaEventsOffline(appSys.offline).getAgendaEvents(week, selector: recurr.testRequest);
+    var recurringEvents = await AgendaEventsOffline(appSys.offline)
+        .getAgendaEvents(week, selector: recurr.testRequest);
     if (recurringEvents != null && recurringEvents.length != 0) {
       recurringEvents.forEach((recurringEvent) {
         events.removeWhere((element) => element.id == recurringEvent.id);
         if (recurringEvent.start != null && recurringEvent.end != null) {
-          recurringEvent.start =
-              DateTime(date.year, date.month, date.day, recurringEvent.start!.hour, recurringEvent.start!.minute);
-          recurringEvent.end =
-              DateTime(date.year, date.month, date.day, recurringEvent.end!.hour, recurringEvent.end!.minute);
+          recurringEvent.start = DateTime(date.year, date.month, date.day,
+              recurringEvent.start!.hour, recurringEvent.start!.minute);
+          recurringEvent.end = DateTime(date.year, date.month, date.day,
+              recurringEvent.end!.hour, recurringEvent.end!.minute);
         }
       });
 
@@ -83,7 +85,8 @@ abstract class API {
   Future<List<Discipline>?> getGrades({bool? forceReload});
 
   ///Get the list of homework only for a specific day (time travel feature)
-  Future<List<Homework>?> getHomeworkFor(DateTime? dateHomework, {bool? forceReload});
+  Future<List<Homework>?> getHomeworkFor(DateTime? dateHomework,
+      {bool? forceReload});
 
   //Get a list of lessons for the agenda part
   ///Get the list of all the next homework (sent by specifics API).
@@ -134,7 +137,8 @@ class AppAccount {
     required this.isParentMainAccount,
     required this.apiType,
   });
-  factory AppAccount.fromJson(Map<String, dynamic> json) => _$AppAccountFromJson(json);
+  factory AppAccount.fromJson(Map<String, dynamic> json) =>
+      _$AppAccountFromJson(json);
   Map<String, dynamic> toJson() => _$AppAccountToJson(this);
 }
 
@@ -167,6 +171,8 @@ class SchoolAccount {
       this.schoolName,
       this.profilePicture})
       : super();
-  factory SchoolAccount.fromJson(Map<String, dynamic> json) => _$SchoolAccountFromJson(json);
+  factory SchoolAccount.fromJson(Map<String, dynamic> json) =>
+      _$SchoolAccountFromJson(json);
   Map<String, dynamic> toJson() => _$SchoolAccountToJson(this);
 }
+//me
